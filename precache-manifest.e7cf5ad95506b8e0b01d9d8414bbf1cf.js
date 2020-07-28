12d722b2e17cb40db18d5b72e9499e80c32df08a
Martin Harrison <mpharrison206@gmail.com>
2020-07-28 21:45:51 +0100

    Updates

---
+++ precache-manifest.e7cf5ad95506b8e0b01d9d8414bbf1cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309bdb4dc0ff9b6eb63122635b0cbc55",
+    "url": "/music-app/index.html"
+  },
+  {
+    "revision": "62b8630477a3a76004fd",
+    "url": "/music-app/static/css/main.e56c7315.chunk.css"
+  },
+  {
+    "revision": "0c790a1f6f3a2cd3cc82",
+    "url": "/music-app/static/js/2.741909fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/music-app/static/js/2.741909fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b8630477a3a76004fd",
+    "url": "/music-app/static/js/main.38338328.chunk.js"
+  },
+  {
+    "revision": "4f04cc2715f387e37000",
+    "url": "/music-app/static/js/runtime-main.726d910a.js"
+  }
+]);
